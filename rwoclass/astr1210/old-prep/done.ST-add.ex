94a157806889eff709588cccb008c159129279ba
rwoconne <rwo@virginia.edu>
2022-04-19 14:56:10 -0400

    041922.Update

---
+++ rwoclass/astr1210/old-prep/done.ST-add.ex
@@ -0,0 +1,13 @@
+
+...From updated Guide19
+
+
+ The recent development of vaccines for the COVID-19 virus is a
+ perfect example of how basic research underpins essential
+ technology. With older technology, it normally took years to perfect
+ viral vaccines. However, the discovery of the structure and function
+ of the "messenger RNA" molecule in 1961 began a chain of research
+ that ultimately led to the very rapid development, in only a few
+ months' time, of COVID-19 vaccines that use mRNA to induce human
+ immune system resistance to the virus.
+
