4a664d791fc4e48080e751af3654779c1aa6d2ab
rwoconne <rwo@virginia.edu>
2024-01-21 23:45:49 -0500

    012124.Update

---
+++ rwoclass/astr1210/old-prep/01-pseudo-extract.ex
@@ -0,0 +1,109 @@
+
+12/7/23; AIs:  check whether is Klass book link in guide now.
+If not, must remove mention in text.  YES, is OK.
+
+Add  "Keep Watching the Skies"  WAS DONE.  
+
+----------------------------------------------------------------
+
+
+
+
+<a name="pseudo">
+<h3>Pseudo-science</h3>
+
+<ul>
+
+"Pseudo-scientific" investigations fall at the margins of science.
+They are often devoted to topics of some popular interest -- such as
+<a href="http://www.badastronomy.com/bad/misc/astrology.html">astrology</a>
+or <a href="guide18.html">"unidentified flying objects" (UFOs)</a> in
+the case of astronomy -- but which have either not yet been validated
+by mainstream science or have been rejected by it.</p>
+
+
+Advocates of pseudo-scientific investigations apply <b>some, but not
+all, of the established methods of science</b>.  These <b>fall
+short</b> of the basic criteria listed above in important ways.  They
+always involve a <b>lack of adequate evidence</b> and frequently
+contradict validated principles of science.  But they have credibility
+for many people because they resemble science, and this opens a large
+arena for human folly.  </p>
+
+We will discuss pseudo-science further, and the "UFO phenomenon" as an
+example, later in <a href="guide18.html">Study Guide 18</a>.</p>
+
+
+<!--  Cut remainder of this section & move to Guide18.  
+
+[https://en.wikipedia.org/wiki/Pseudoscience]  -->
+
+Prominent <b>examples</b> of pseudoscience: astrology, extra-sensory
+perception (ESP), psychic forecasts, telekinesis, UFO abductions,
+ghosts, the Bermuda Triangle, Atlantis and other ancient
+super-civilizations, personal reinterpretations of major scientific
+theories like relativity or quantum mechanics.  Also, much
+"alternative" medicine.  </p>
+
+
+Hallmarks: uncritical acceptance of poorly documented phenomena; lack
+of experiments or inadequate experimental controls; inexperienced
+observers; logical fallicies; non-repeatability; often self-deception;
+often for-profit publicity/entertainment; sometimes deliberate fraud.</p>
+
+The evidence is <b>always inadequate</b>: 
+</p>
+
+<ul>
+
+The issue is not that there are no good scientific <b>explanations</b>
+for claimed pseudo-scientific phenomena.  At any time, there
+are <b>thousands of well-documented phenomena</b> that science does
+not properly understand.  These are what active scientists devote
+their careers to studying.  By contrast, in the case of pseudo-science
+many of the phenomena themselves have <b>not been demonstrated to
+exist by objective experimentation or observational evidence</b>.</p>
+
+<ul> For instance, we <b>know</b> that penicillin kills certain types
+of dangerous bacteria.  But no one has ever convincingly been shown to
+be able to read someone else's mind.</p> </ul>
+
+If credible evidence emerges
+for any of the marginal "pseudo" areas, they will then receive
+mainstream scientific attention.   </p>
+
+</ul>
+
+A <b>central failure</b> of pseudo-science is that it almost always involves
+a <b>lack of consistency</b> with established scientific principles or facts
+(e.g. foreseeing the future contradicts the verified principles of
+special relativity).  </p>
+
+
+Most pseudo-science is harmless, except for the sloppy thinking and
+miseducation it encourages.  But some pseudo-science poses tangible
+<b>threats</b> to you and society.  For example:</p>
+
+<ul>
+
+Fraudulent or ineffective "alternative" health-care (such
+as <a href="http://www.youtube.com/watch?v=HMGIbOGu8q0&feature=related">homeopathy</a>,
+in which the active therapeutic ingredients are diluted to the point
+where not a single molecule remains).</p>
+
+Rejection of proven beneficial medical protocols or treatments, like
+vaccines, because of personal or political agendas.  These were
+especially harmful, and resulted in the unnecessary deaths of hundreds
+of thousands of people in the US alone, during the
+<a href="https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9282830/"> COVID-19
+pandemic</a>.  </p>
+
+<a href="https://www.washingtonpost.com/news/acts-of-faith/wp/2017/08/24/accused-of-satanism-they-spent-21-years-in-prison-they-were-just-declared-innocent-and-were-paid-millions/?tid=hybrid_experimentrandom_3_na&utm_term=.b0fc7e4044ee">False
+criminal prosecutions</a> (murder, child abuse, satanic rituals, etc.)
+based on erroneous psychotherapy (e.g.
+<a href="im/recovered-memory-alien-Doonesbury.gif">
+"suppressed" memories</a> "recovered" by hypnosis).  </p>
+
+</ul> 
+
+</ul> </ul> 
